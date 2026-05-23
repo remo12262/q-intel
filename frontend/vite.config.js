6514174d470dc331f009037f1e74d725ca79ed58
Remo Pulcini <remo@email.it>
2026-05-23 18:41:37 +0200

    Add build target es2015 to vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2015'
+  },
   server: { proxy: { '/api': 'http://localhost:8000' } }
 })
